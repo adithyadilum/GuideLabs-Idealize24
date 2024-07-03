cf97cffb7ae610e49c989524fd25cbca8b4d83fe
dilum-20 <adithyadilum11@gmail.com>
2024-07-04 00:07:56 +0530

    chore: Remove unused Result component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import Hero from './Hero.jsx';
 import Question from './Question.jsx';
-import Result from './Result.jsx';
+// import Result from './Result.jsx';
 
 function App() {
     return (
         <div>
             <Hero />
             <Question />
-            <Result />
+            {/* <Result /> */}
         </div>
     )
 }
